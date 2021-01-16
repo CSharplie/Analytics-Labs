912e2f94067402a365703913d0501e4cc075c372
CSharplie <charlie.collier@outlook.fr>
2021-01-16 11:00:15 +0100

    Add module logic

---
+++ AzureDevops-ADF/resources/resources.tf
@@ -0,0 +1,76 @@
+
+variable "prefix" {}
+variable "location" {}
+variable "environment" {}
+variable "ip" {}
+variable "db_username" {}
+variable "db_password" {}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "adf_hands_on_${var.environment}"
+  location = var.location
+}
+
+resource "azurerm_sql_server" "dbserver" {
+  name                         = "${var.prefix}-sql-${var.environment}" 
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = var.location
+  version                      = "12.0"
+  administrator_login          = var.db_username
+  administrator_login_password = var.db_password
+}
+
+resource "azurerm_sql_database" "db" {
+  name                = "${var.prefix}-db-${var.environment}" 
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  server_name         = azurerm_sql_server.dbserver.name
+}
+
+resource "azurerm_sql_firewall_rule" "db_fw" {
+  name                = "Current User"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_sql_server.dbserver.name
+  start_ip_address    = var.ip
+  end_ip_address      = var.ip
+}
+
+resource "azurerm_sql_firewall_rule" "db_fw_az" {
+  name                = "Azure Services"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_sql_server.dbserver.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
+
+resource "azurerm_data_factory" "adf" {
+  name                = "${var.prefix}-adf-${var.environment}" 
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_key_vault" "akv" {
+  name                        = "${var.prefix}-akv-${var.environment}"
+  location                    = var.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  sku_name                    = "standard"
+}
---
+++ AzureDevops-ADF/setup.tf
@@ -0,0 +1,44 @@
+variable "prefix" {
+    type = string
+    description = "Prefix for resources names"
+}
+
+variable "db_username" {
+    type = string
+    description = "Database admin account"
+}
+
+variable "db_password" {
+    type = string
+    description = "Database admin password"
+}
+
+variable "location" {
+	default = "westeurope"
+}
+
+data "http" "myip" {
+  url = "http://ipv4.icanhazip.com"
+}
+
+module "acm_request_certificate_example" {
+  source                            = "./resources"
+
+  prefix                            = var.prefix
+  db_username                       = var.db_username
+  db_password                       = var.db_password
+  location                          = var.location
+  ip                                = chomp(data.http.myip.body)
+  environment                       = "dev"
+}
+
+module "acm_request_certificate_other_example" {
+  source                            = "./resources"
+
+  prefix                            = var.prefix
+  db_username                       = var.db_username
+  db_password                       = var.db_password
+  location                          = var.location
+  ip                                = chomp(data.http.myip.body)
+  environment                       = "tst"
+}
