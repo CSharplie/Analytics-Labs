9847bacbe81dc16a95b242629b9a8d34074e4543
CSharplie <charlie.collier@outlook.fr>
2021-01-31 15:47:15 +0100

    Set harcoded password

--- AzureDevops-ADF/setup.tf
+++ AzureDevops-ADF/setup.tf
@@ -1,42 +1,48 @@
 variable "prefix" {
-    type = string
-    description = "Prefix for resources names"
+	type 				= string
+	description 		= "Enter your trigram in lowercase (ex: Julien Schwarzer = jsc)"
+
+	validation {
+		condition	 	= can(regex("^[a-z]{3}$", var.prefix))
+		error_message 	= "Please enter a value in lowercase with 3 characters."
+	}
 }
 
 variable "db_username" {
-    type = string
-    description = "Database admin account"
+	type 				= string
+	default 			= "HandsOn"
 }
 
 variable "db_password" {
-    type = string
-    description = "Database admin password"
+	type 				= string
+	default 			= "PixelAnalytics$$92130"
 }
 
 variable "location" {
-	default = "westeurope"
+	type 				= string
+	default 			= "westeurope"
 }
 
 data "http" "myip" {
-  url = "http://ipv4.icanhazip.com"
+	url 				= "http://ipv4.icanhazip.com"
 }
 
-module "acm_request_certificate_example" {
-  source                            = "./resources"
-  prefix                            = var.prefix
-  db_username                       = var.db_username
-  db_password                       = var.db_password
-  location                          = var.location
-  ip                                = chomp(data.http.myip.body)
-  environment                       = "dev"
+module "resources_dev" {
+	source				= "./resources"
+	prefix				= var.prefix
+	db_username			= var.db_username
+	db_password			= var.db_password
+	location			= var.location
+	ip					= chomp(data.http.myip.body)
+	environment			= "dev"
 }
 
-module "acm_request_certificate_other_example" {
-  source                            = "./resources"
-  prefix                            = var.prefix
-  db_username                       = var.db_username
-  db_password                       = var.db_password
-  location                          = var.location
-  ip                                = chomp(data.http.myip.body)
-  environment                       = "tst"
+module "resources_tst" {
+	source				= "./resources"
+	prefix				= var.prefix
+	db_username			= var.db_username
+	db_password			= var.db_password
+	location			= var.location
+	ip					= chomp(data.http.myip.body)
+	environment			= "tst"
 }
