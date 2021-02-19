79dc3cd33c14ab26630b679beb2a626f4c386ba6
CSharplie <charlie.collier@outlook.fr>
2021-02-19 14:27:48 +0100

    Rename CD/CD folder

--- AzureDevops-ADF/setup.tf
+++ AzureDevops-ADF/setup.tf
@@ -1,48 +1,28 @@
 variable "prefix" {
-  type              = string
-  description       = "Enter your trigram in lowercase (ex: Julien Schwarzer = jsc)"
+  type                = string
+  description         = "Enter your trigram in lowercase (ex: Julien Schwarzer = jsc)"
 
   validation {
-    condition       = can(regex("^[a-z]{3}$", var.prefix))
-    error_message   = "Please enter a value in lowercase with 3 characters."
+    condition         = can(regex("^[a-z]{3}$", var.prefix))
+    error_message     = "Please enter a value in lowercase with 3 characters."
   }
 }
 
-variable "db_username" {
-  type              = string
-  default           = "HandsOn"
-}
-
-variable "db_password" {
-  type              = string
-  default           = "PixelAnalytics$$92130"
-}
-
 variable "location" {
-  type              = string
-  default           = "westeurope"
-}
-
-data "http" "myip" {
-  url               = "http://ipv4.icanhazip.com"
+  default             = "westeurope"
 }
 
 module "resources_dev" {
-  source            = "./resources"
-  prefix            = var.prefix
-  db_username       = var.db_username
-  db_password       = var.db_password
-  location          = var.location
-  ip                = chomp(data.http.myip.body)
-  environment       = "dev"
+  source              = "./resources"
+  prefix              = var.prefix
+  location            = var.location
+  environment         = "dev"
 }
 
 module "resources_tst" {
-  source            = "./resources"
-  prefix            = var.prefix
-  db_username       = var.db_username
-  db_password       = var.db_password
-  location          = var.location
-  ip                = chomp(data.http.myip.body)
-  environment       = "tst"
+  source              = "./resources"
+  prefix              = var.prefix
+  location            = var.location
+  environment         = "tst"
 }
+
