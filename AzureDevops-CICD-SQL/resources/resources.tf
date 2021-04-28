ea1753de1444bee47af2341a675ffc438897a607
CSharplie <charlie.collier@outlook.fr>
2021-04-28 20:03:31 +0200

    Add prefix to ressource groupe

--- AzureDevops-CICD-SQL/resources/resources.tf
+++ AzureDevops-CICD-SQL/resources/resources.tf
@@ -22,7 +22,7 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "rg" {
-  name                                = "adf_hands_on_${var.environment}"
+  name                                = "sql_hands_on_${var.prefix}_${var.environment}"
   location                            = var.location
 }
 
